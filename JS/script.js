34775f2f6de9c5f89a518771d18fc54bfe1da95e
Queen-Uwitonze <uwitonzeq@gmail.com>
2018-12-21 09:31:05 -0500

    show the message as an alert meassage

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,16 @@
-function mytest()
-{
-var question1=document.forms["myform"]["choice1"].value;
-var question2=document.forms["myform"]["choice2"].value;
-var question3=document.forms["myform"]["choice3"].value;
-var question4=document.forms["myform"]["choice4"].value;
-var marks=0;
-if(question1=="B")
-{
-   marks=marks+10;
-   }
 
-if(question2=="D")
-{
-   marks=marks+10;
-   }
-if(question3=="B")
-{
-   marks=marks+10;
-}
-if(question4=="C")
-{
-   marks=marks+10;
-}
-alert("your score is:"+marks);
-}
+
+$(document).ready(function (){
+    $('#blanks form').submit(function(event){
+    var marks=0;
+      var blanks=["choice1", "choice2","choice3","choice4"]
+        blanks.forEach(function(blank) {
+            var userInput = $("input:radio[name=" + blank + "]:checked" ).val();
+                    if(userInput=="B"){
+                    marks = marks+10;
+                    }
+                })
+          alert(marks)
+    event.preventDefault();
+ });
+ });
