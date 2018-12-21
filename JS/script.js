073852f55a43f73d5cd4be6a188d306d72428639
Queen-Uwitonze <uwitonzeq@gmail.com>
2018-12-21 03:57:50 -0500

    added function and related result to question asked

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,25 @@
-$(document).ready(function() {
-    var choices = $('.choice');
-
-    choices.on('click', function(event) {
-        var choice = $(event.target);
-        choice
-            .find('[name="choice"]')
-            .prop('checked', true)
-            .trigger('change');
-    });
-
-    var inputs = $('.choice input');
-    inputs.on('change', function(event) {
-        var input = $(event.target);
-        var choice = $(this).closest('.choice');
-
-        $('.choice.active').removeClass('active');
-        choice.addClass('active');
-    });
-});​
+function mytest()
+{
+var question1=document.forms["myform"]["choice1"].value;
+var question2=document.forms["myform"]["choice2"].value;
+var question3=document.forms["myform"]["choice3"].value;
+var question4=document.forms["myform"]["choice4"].value;
+var marks=0;
+if(question1=="B")
+{
+   marks=marks+10;
+   }
+if(question2=="D")
+{
+   marks=marks+10;
+   }
+if(question3=="B")
+{
+   marks=marks+10;
+}
+if(question4=="C")
+{
+   marks=marks+10;
+}
+alert("your score is:"+marks);
+}
